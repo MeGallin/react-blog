dab67a074e4e7989b2d7aa064d4cb1cc0bf6988e
MeGallin <me@garyallin.uk>
2021-08-09 16:58:50 +0100

    basic routes working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
+import Home from './views/home/Home';
 import Footer from './components/footer/Footer';
+import About from './views/about/About';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/blog/homeBlog/HomeBlog.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HomeBlog.css';
+
+function HomeBlog() {
+  return <div>Home blog blogs here we will run the http request</div>;
+}
+
+export default HomeBlog;
