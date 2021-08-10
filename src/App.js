9eb2399eabeffaeffe89ca538d3785a97fd3d776
MeGallin <me@garyallin.uk>
2021-08-10 12:37:22 +0100

    get request working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './views/home/Home';
@@ -8,20 +10,22 @@ import About from './views/about/About';
 
 function App() {
   return (
-    <Router>
-      <div id="app-wrapper">
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div id="app-wrapper">
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
