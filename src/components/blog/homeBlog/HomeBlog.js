079a3248ca5abe3568be63e4191acd01f8b8fdf6
MeGallin <me@garyallin.uk>
2021-09-02 12:07:41 +0100

    tidy up modal

--- src/components/blog/homeBlog/HomeBlog.js
+++ src/components/blog/homeBlog/HomeBlog.js
@@ -17,8 +17,7 @@ function HomeBlog({
   postDisLikeRequest,
 }) {
   const [searchField, setSearchField] = useState('');
-  const [showFullBlog, setShowFullBlog] = useState(false);
-  const [modalData, setModalData] = useState({});
+
   const [isDisabled, setIsDisabled] = useState(true);
 
   useEffect(() => {
@@ -33,7 +32,7 @@ function HomeBlog({
     return blog.heading.toLowerCase().includes(searchField.toLowerCase());
   });
 
-  const handleLike = (id, name, heading, message, posted, likes, dislikes) => {
+  const handleLike = (id, name, heading, message, posted, likes) => {
     const data = {
       id: id,
       name: name,
@@ -41,21 +40,11 @@ function HomeBlog({
       message: message,
       posted: posted,
       likes: parseInt(likes) + 1,
-      dislikes: dislikes,
     };
-    setModalData(data);
     postLikeRequest(data);
   };
 
-  const handleDisLike = (
-    id,
-    name,
-    heading,
-    message,
-    posted,
-    dislikes,
-    likes,
-  ) => {
+  const handleDisLike = (id, name, heading, message, posted, dislikes) => {
     const data = {
       id: id,
       name: name,
@@ -63,19 +52,10 @@ function HomeBlog({
       message: message,
       posted: posted,
       dislikes: parseInt(dislikes) + 1,
-      likes: likes,
     };
-    setModalData(data);
     postDisLikeRequest(data);
   };
 
-  const handleViewBlog = (index) => {
-    setShowFullBlog(true);
-    setModalData(filteredBlogs[index]);
-  };
-  const handleCloseViewBlog = () => {
-    setShowFullBlog(false);
-  };
   const matchName = (current) => {
     let reggie = new RegExp(searchField, 'ig');
     let found = current.search(reggie) !== -1;
@@ -89,120 +69,132 @@ function HomeBlog({
         );
   };
 
+  // Modal Stuff
+  const modalContent = (
+    id,
+    heading,
+    message,
+    posted,
+    name,
+    likes,
+    dislikes,
+    admin,
+  ) => {
+    return (
+      <div>
+        <h1 dangerouslySetInnerHTML={{ __html: heading }} />
+        <p dangerouslySetInnerHTML={{ __html: message }} />
+
+        <p className="userDetails">
+          Post by: {name} {timeStamp(posted, 'YYYYMMDD, h:mm:ss a')}
+        </p>
+        <hr />
+        <div className="clap-wrapper">
+          <div>
+            <span
+              className="likes"
+              onClick={() =>
+                handleLike(id, name, heading, message, posted, likes)
+              }
+            >
+              <i className="far fa-thumbs-up"></i>
+              <span className="text-small">{likes}</span>
+            </span>
+          </div>
+          <div>
+            <span
+              className="dislikes"
+              onClick={() =>
+                handleDisLike(id, name, heading, message, posted, dislikes)
+              }
+            >
+              {' '}
+              <span className="text-small">{dislikes}</span>
+              <i className="far fa-thumbs-down"></i>
+            </span>
+          </div>
+        </div>
+        <hr />
+        {admin === '1' ? (
+          <div className="adminMessage">
+            <i className="fas fa-exclamation"></i> Edited by Admin
+          </div>
+        ) : null}
+      </div>
+    );
+  };
+  const buttonLabel = () => {
+    return <span>Read more . . .</span>;
+  };
+  // Modal Stuff
+
   return blogs.loading ? (
     <h2>Loading </h2>
   ) : blogs.error ? (
     <h2>{blogs.error}</h2>
   ) : (
     <div>
-      {showFullBlog ? (
-        <div className="modal__wrapper">
-          <Modal handleCloseViewBlog />
-          <h3>{modalData.heading}</h3>
-          <p dangerouslySetInnerHTML={{ __html: modalData.message }} />
-          <p>{timeStamp(modalData.posted, 'YYYYMMDD, h:mm:ss a')}</p>
-          <p>{modalData.name}</p>
-          <div className="clap-wrapper">
-            <div>
-              <span
-                className="likes"
-                onClick={() =>
-                  handleLike(
-                    modalData.id,
-                    modalData.name,
-                    modalData.heading,
-                    modalData.message,
-                    modalData.posted,
-                    modalData.likes,
-                    modalData.dislikes,
-                  )
-                }
-              >
-                <i className="far fa-thumbs-up"></i>
-                <span className="text-small">{modalData.likes}</span>
-              </span>
-            </div>
-            <div>
-              <span
-                className="dislikes"
-                onClick={() =>
-                  handleDisLike(
-                    modalData.id,
-                    modalData.name,
-                    modalData.heading,
-                    modalData.message,
-                    modalData.posted,
-                    modalData.dislikes,
-                    modalData.likes,
-                  )
-                }
-              >
-                <i className="far fa-thumbs-down"></i>
-                <span className="text-small">{modalData.dislikes}</span>
-              </span>
-            </div>
-          </div>
-          <button type="button" onClick={() => handleCloseViewBlog()}>
-            X
-          </button>
-        </div>
-      ) : null}
-      <div className={showFullBlog ? 'blog-wrapper' : null}>
-        <SearchInput
-          type="search"
-          placeholder="Search a Title..."
-          handleSearch={(e) =>
-            setSearchField(e.target.value, setIsDisabled(false))
-          }
-          className={searchField.length < 3 ? 'invalid' : 'entered'}
-          value={searchField}
-        />
-        <p></p>
-        <button
-          onClick={() => setSearchField('', setIsDisabled(true))}
-          disabled={isDisabled}
-        >
-          Clear Search
-        </button>
-        <hr />
+      <SearchInput
+        type="search"
+        placeholder="Search a Title..."
+        handleSearch={(e) =>
+          setSearchField(e.target.value, setIsDisabled(false))
+        }
+        className={searchField.length < 3 ? 'invalid' : 'entered'}
+        value={searchField}
+      />
+      <p></p>
+      <button
+        onClick={() => setSearchField('', setIsDisabled(true))}
+        disabled={isDisabled}
+      >
+        Clear Search
+      </button>
+      <hr />
 
-        {filteredBlogs &&
-          filteredBlogs &&
-          filteredBlogs.map((blog, index) => (
-            <React.Fragment key={blog.id}>
-              <div>
-                <h1
-                  className="search-title"
-                  onClick={() => handleViewBlog(index)}
-                  dangerouslySetInnerHTML={{ __html: matchName(blog.heading) }}
-                />
+      {filteredBlogs &&
+        filteredBlogs &&
+        filteredBlogs.map((blog, index) => (
+          <React.Fragment key={blog.id}>
+            <div>
+              <h1
+                className="search-title"
+                dangerouslySetInnerHTML={{ __html: matchName(blog.heading) }}
+              />
 
-                <div className="blog-content-wrapper">
-                  <div className="text-wrapper">
-                    <p
-                      dangerouslySetInnerHTML={{
-                        __html: blog.message.slice(0, 200) + '...',
-                      }}
-                    ></p>
-                    <span
-                      onClick={() => handleViewBlog(index)}
-                      className="read-more"
-                    >
-                      read more...
-                    </span>
-                  </div>
-                  <p>POST BY: {blog.name}</p>
+              <div className="blog-content-wrapper">
+                <div>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: blog.message.slice(0, 200) + '...',
+                    }}
+                  ></p>
+                  <p className="userDetails">Post by: {blog.name}</p>
+                  <Modal
+                    title={buttonLabel()}
+                    content={modalContent(
+                      blog.id,
+                      blog.heading,
+                      blog.message,
+                      blog.posted,
+                      blog.name,
+                      blog.likes,
+                      blog.dislikes,
+                      blog.admin,
+                    )}
+                  />
                 </div>
-                {blog.admin === '1' ? (
-                  <div className="adminMessage">
-                    <i className="fas fa-exclamation"></i> Edited by Admin
-                  </div>
-                ) : null}
               </div>
-              <hr />
-            </React.Fragment>
-          ))}
-      </div>
+
+              {blog.admin === '1' ? (
+                <div className="adminMessage">
+                  <i className="fas fa-exclamation"></i> Edited by Admin
+                </div>
+              ) : null}
+            </div>
+            <hr />
+          </React.Fragment>
+        ))}
     </div>
   );
 }
