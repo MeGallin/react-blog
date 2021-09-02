7d9fbdcc943fd7cb3a14dce8d40622a146c0a9bf
MeGallin <me@garyallin.uk>
2021-09-02 14:53:16 +0100

    contact form modal

---
+++ src/components/contactForm/ContactForm.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import './ContactForm.css';
+import FormInputs from '../formInputs/FormInputs';
+
+const nameRegEx = /^(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{2,}$/;
+const emailRegEx =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+function ContactForm() {
+  const [legend] = useState('Contact form');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name,
+      email,
+      message,
+    };
+    console.log(data);
+  };
+
+  return (
+    <React.Fragment>
+      <fieldset className="fieldSet">
+        <legend>{legend}</legend>
+        <form onSubmit={handleSubmit}>
+          <div></div>
+          <div>
+            <FormInputs
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              name="name"
+              required
+              className={!nameRegEx.test(name) ? 'invalid' : 'entered'}
+              error={
+                !nameRegEx.test(name) && name.length !== 0
+                  ? `Name field must start with an uppercase letter and contain at least 3 letters and have no white space.`
+                  : null
+              }
+            />
+          </div>
+          <div>
+            <FormInputs
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              name="email"
+              required
+              className={!emailRegEx.test(email) ? 'invalid' : 'entered'}
+              error={
+                !emailRegEx.test(email) && email.length !== 0
+                  ? `Invalid email address.`
+                  : null
+              }
+            />
+          </div>
+
+          <div>
+            <label htmlFor="message">
+              Blog
+              <textarea
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                type="text"
+                name="message"
+                className={message.length < 10 ? 'invalid' : 'entered'}
+              />
+            </label>
+          </div>
+          <button
+            type="submit"
+            value="submit"
+            disabled={
+              !nameRegEx.test(name) ||
+              !emailRegEx.test(email) ||
+              message.length < 10
+            }
+          >
+            Submit
+          </button>
+        </form>
+      </fieldset>
+    </React.Fragment>
+  );
+}
+
+export default ContactForm;
