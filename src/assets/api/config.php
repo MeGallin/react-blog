d0cedf3e0c5373ad85667ede1c63d676c481a52e
MeGallin <me@garyallin.uk>
2021-09-02 15:43:39 +0100

    config settings

--- src/assets/api/config.php
+++ src/assets/api/config.php
@@ -4,9 +4,9 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 $serverName = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "blog";
+$username   = "livenoti_meGallin";
+$password   = "meGallin248!!";
+$dbname     = "livenoti_vue-blog";
 // Create connection
 $conn = new mysqli($serverName, $username, $password, $dbname);
 // Check connection
